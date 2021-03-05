d6262ed8e094a625292fb5f955dcd97541ed8a39
yyamada12 <12yacropolisy@gmail.com>
2021-03-05 09:24:43 +0900

    Operator functions and functions with vararg parameters

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,22 @@
 fun main() {
-    infix fun Int.times(str: String) = str.repeat(this)
-    println(2 times "Bye ")
+    operator fun Int.times(str: String) = str.repeat(this)
+    println(2 * "Bye ")
 
-    val pair = "Ferrai" to "Katrina"
-    println(pair)
+    operator fun String.get(range: IntRange) = substring(range)
+    val str = "Always forgive your enemies; nothing annoys them so much."
+    println(str[0..14])
 
-    infix fun String.onto(other: String) = Pair(this, other)
-    val myPair = "McLaren" onto "Lucas"
-    println(myPair)
-
-
-    val sophia = Person("Sophia")
-    val claudia = Person("Claudia")
-    sophia likes claudia
-
-    for  (p in sophia.likedPeople) {
-        println(p.name)
+    fun printAll(vararg messages: String) {
+        for (m in messages) println(m)
     }
-}
+    printAll("Hello", "Hallo", "Salut", "Hola", "你好")
 
-class Person(val name: String) {
-    val likedPeople = mutableListOf<Person>()
-    infix fun likes(other: Person) {likedPeople.add(other)}
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {
+        for (m in messages) println(prefix + m)
+    }
+    printAllWithPrefix("Hello", "Hallo", "Salut", "Hola", "你好", prefix="Greeting: ")
+
+    fun log(vararg entries: String) {
+        printAll(*entries)
+    }
 }
