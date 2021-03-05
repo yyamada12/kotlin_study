397c773853ce415bbd3e955accab12d0231ffa1c
yyamada12 <12yacropolisy@gmail.com>
2021-03-06 08:59:07 +0900

    inheritance

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,12 @@
+open class Lion(val name: String, val origin: String) {
+    fun sayHello() {
+        println("$name, the lion from $origin syas: graoh!")
+    }
+}
+
+class Asiatic(name: String) : Lion(name = name, origin="India")
+
 fun main() {
-    operator fun Int.times(str: String) = str.repeat(this)
-    println(2 * "Bye ")
-
-    operator fun String.get(range: IntRange) = substring(range)
-    val str = "Always forgive your enemies; nothing annoys them so much."
-    println(str[0..14])
-
-    fun printAll(vararg messages: String) {
-        for (m in messages) println(m)
-    }
-    printAll("Hello", "Hallo", "Salut", "Hola", "你好")
-
-    fun printAllWithPrefix(vararg messages: String, prefix: String) {
-        for (m in messages) println(prefix + m)
-    }
-    printAllWithPrefix("Hello", "Hallo", "Salut", "Hola", "你好", prefix="Greeting: ")
-
-    fun log(vararg entries: String) {
-        printAll(*entries)
-    }
+    val lion: Lion = Asiatic("Rufo")
+    lion.sayHello()
 }
