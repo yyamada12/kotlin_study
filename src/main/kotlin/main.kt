c4ab354309cfff09a39d8f689a4572ea0d95d0af
yyamada12 <12yacropolisy@gmail.com>
2021-03-05 09:08:06 +0900

    Infix Functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,25 @@
 fun main() {
-    println("What's your name?")
-    val name = readLine()
-    println("Hello, $name!")
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(2 times "Bye ")
+
+    val pair = "Ferrai" to "Katrina"
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    sophia likes claudia
+
+    for  (p in sophia.likedPeople) {
+        println(p.name)
+    }
+}
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) {likedPeople.add(other)}
 }
