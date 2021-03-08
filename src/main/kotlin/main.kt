a4987f802b692dacfa51aaaa1597dae7ce754027
yyamada12 <12yacropolisy@gmail.com>
2021-03-08 14:37:16 +0900

    scope functions apply

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,17 @@
-data class User(val name: String, val id: Int)
+data class Person(var name: String, var age: Int, var about: String) {
+    constructor() : this("", 0, "")
+}
 
 fun main() {
-    val user = User("Alex", 1)
-    println(user)
+    val jake = Person()
+    println(jake)
 
-    val secondUser = User("Alex", 1)
-    val thirdUser = User("Max", 2)
+    val stringDescription = jake.apply {
+        name = "jake"
+        this.age = 30
+        about = "Android developer"
+    }.toString()
 
-    println("user == secondUser: ${user == secondUser}")
-    println("user == thirdUser: ${user == thirdUser}")
-
-    println(user.copy())
-    println(user.copy("Max"))
-    println(user.copy(id = 2))
-
-    val forthUser = user.copy("Lax", 3)
-    println(user)
-    println(forthUser)
-
-    println("name = ${user.component1()}")
-    println("id = ${user.component2()}")
+    println(jake)
+    println(stringDescription)
 }
